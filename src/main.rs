f118edf5e36a7c833dbdd2b204c93b2873850b81
Ragnar-D5 <erik.scheidig@gmx.de>
2023-09-11 15:53:48 +0200

    added root struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use iced::Sandbox, Element;
+
+struct Planer;
+
+struct Message;
+
+impl Sandbox for Planer {
+    type Message = Message
+
+    fn new() -> Self {
+        Planer
+    }
+
+    fn title(&self) -> String {
+        "Planer"
+    }
+
+    fn update (&mut self, Message) {
+
+    }
+
+    fn view(&self) -> Element<Message> {
+        
+    }
 }
